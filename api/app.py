911f67413c66620750979662fd80a5ea9a41859b
Leandro Correa dos Santos <leandro.admo@gmail.com>
2020-05-30 23:48:24 -0300

    Revert "Feature/users"

--- api/app.py
+++ api/app.py
@@ -3,45 +3,24 @@ import os
 from api import app as application
 from api.v1.resources import CategoriasView as v1_Categorias
 from api.v1.resources import NoticiasView as v1_Noticias
-from api.v1.user_resources import LoginView as v1_Login
-from api.v1.user_resources import UserView as v1_User
 
 
 #  This rule below is an example and can be removed.
 application.add_url_rule(
-    '/v1/categoria',
+    '/v1/categorias',
     view_func=v1_Categorias.as_view('categorias'),
-    methods=['GET', 'POST']
+    methods=['GET']
 )
 
 application.add_url_rule(
-    '/v1/categoria/<categoria_id>',
+    '/v1/categoria',
     view_func=v1_Categorias.as_view('categoria'),
-    methods=['PUT', 'DELETE']
-)
+    methods=['POST'])
 
 application.add_url_rule(
     '/v1/noticias',
     view_func=v1_Noticias.as_view('noticias'),
     methods=['GET']
-)
-
-application.add_url_rule(
-    '/v1/login',
-    view_func=v1_Login.as_view('login'),
-    methods=['POST']
-)
-
-application.add_url_rule(
-    '/v1/user',
-    view_func=v1_User.as_view('users'),
-    methods=['GET', 'POST']
-)
-
-application.add_url_rule(
-    '/v1/user/<user_id>',
-    view_func=v1_User.as_view('user'),
-    methods=['PUT', 'DELETE']
 )
 
 
--- api/manager.py
+++ api/manager.py
@@ -1,11 +1,11 @@
 import sys
 import errno
+from datetime import datetime
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from werkzeug.security import generate_password_hash
 
 from api import app, db
-from api.models import Role, User, UserRoles
+from api.models import Role, User
 
 
 migrate = Migrate(app, db)
@@ -17,42 +17,27 @@ manager.add_command('db', MigrateCommand)
 @manager.command
 def seed():
     roles = [
-        {"id": 1, "name": "admin"},
-        {"id": 2, "name": "editor"},
-        {"id": 3, "name": "jornalista"},
+        {"id": 1, "nome": "admin"},
+        {"id": 1, "nome": "editor"},
+        {"id": 1, "nome": "jornalista"},
     ]
 
     for role in roles:
         try:
-            db.session.add(Role(**role))
+            db.session.add(Role(role))
             db.session.commit()
         except Exception as e:
-            db.session.rollback()
             print("Erro ao cadastrar grupo: {}".format(e))
 
     user = {
         "id": 1,
         "username": "admin",
-        "password": generate_password_hash("12345678"),
+        "password": "12345678",
         "email": "admin@localhost",
         "first_name": "admin",
         "last_name": "admin",
+        "roles": [{"id": 1, "nome": "admin"}],
     }
-
-    try:
-        db.session.add(User(**user))
-        db.session.commit()
-    except Exception as e:
-        db.session.rollback()
-        print("Erro ao cadastrar administrador: {}".format(e))
-
-    userroles = {"id": 1, "user_id": 1, "role_id": 1}
-    try:
-        db.session.add(UserRoles(**userroles))
-        db.session.commit()
-    except Exception as e:
-        db.session.rollback()
-        print("Erro ao cadastrar relacionamento: {}".format(e))
 
 
 if __name__ == '__main__':
